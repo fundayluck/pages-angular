2f7101628c4243530cbc751943ceb77d67082ca8
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-12 08:33:26 +0700

    Accessing elements from a custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ClassDirective } from './class.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ClassDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,11 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
-  standalone: true
+  standalone: true,
 })
 export class ClassDirective {
+  private _elRef: ElementRef<HTMLElement>;
+  constructor(el: ElementRef) {
+    this._elRef = el;
 
-  constructor() { }
-
+    this._elRef.nativeElement.style.backgroundColor = 'orange';
+  }
 }
