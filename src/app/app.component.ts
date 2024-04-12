3ebc0f9ae4174ccd6e0d85671cd265607afe5b37
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-12 07:58:21 +0700

    wrapup pages with ng-container like fragment in react, adding logic pages view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,5 +32,69 @@ export class AppComponent {
       title: 'my dark side :P',
       url: 'https://images.unsplash.com/photo-1597739239353-50270a473397?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG93bnxlbnwwfHwwfHx8MA%3D%3D',
     },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+      title: 'At the Town',
+      url: 'https://media.istockphoto.com/id/952083480/photo/panorama-of-skyscrapers-in-a-modern-city.webp?b=1&s=170667a&w=0&k=20&c=_cLMVZVE35TmuAr17SL8qN4E1LzFRN3lWrdvhXhexgE=',
+    },
+    {
+      title: 'with friends',
+      url: 'https://media.istockphoto.com/id/917742634/photo/sweet-friendships-refreshes-the-soul.webp?b=1&s=170667a&w=0&k=20&c=zt_y5sIIdPrn7ksWO0aVfDtpbgNP2rt2bx-TEwZXr6s=',
+    },
+    {
+      title: 'Here we go again!',
+      url: 'https://images.unsplash.com/photo-1543872084-c7bd3822856f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'my dark side :P',
+      url: 'https://images.unsplash.com/photo-1597739239353-50270a473397?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG93bnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+      title: 'At the Town',
+      url: 'https://media.istockphoto.com/id/952083480/photo/panorama-of-skyscrapers-in-a-modern-city.webp?b=1&s=170667a&w=0&k=20&c=_cLMVZVE35TmuAr17SL8qN4E1LzFRN3lWrdvhXhexgE=',
+    },
+    {
+      title: 'with friends',
+      url: 'https://media.istockphoto.com/id/917742634/photo/sweet-friendships-refreshes-the-soul.webp?b=1&s=170667a&w=0&k=20&c=zt_y5sIIdPrn7ksWO0aVfDtpbgNP2rt2bx-TEwZXr6s=',
+    },
+    {
+      title: 'Here we go again!',
+      url: 'https://images.unsplash.com/photo-1543872084-c7bd3822856f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'my dark side :P',
+      url: 'https://images.unsplash.com/photo-1597739239353-50270a473397?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG93bnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+      title: 'At the Town',
+      url: 'https://media.istockphoto.com/id/952083480/photo/panorama-of-skyscrapers-in-a-modern-city.webp?b=1&s=170667a&w=0&k=20&c=_cLMVZVE35TmuAr17SL8qN4E1LzFRN3lWrdvhXhexgE=',
+    },
+    {
+      title: 'with friends',
+      url: 'https://media.istockphoto.com/id/917742634/photo/sweet-friendships-refreshes-the-soul.webp?b=1&s=170667a&w=0&k=20&c=zt_y5sIIdPrn7ksWO0aVfDtpbgNP2rt2bx-TEwZXr6s=',
+    },
+    {
+      title: 'Here we go again!',
+      url: 'https://images.unsplash.com/photo-1543872084-c7bd3822856f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'my dark side :P',
+      url: 'https://images.unsplash.com/photo-1597739239353-50270a473397?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG93bnxlbnwwfHwwfHx8MA%3D%3D',
+    },
   ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5
+  }
 }
